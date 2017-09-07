2318a89447bb78e0a7a475cc71adbfdf9d494da4
dpdmseh <dpdmseh@naver.com>
2017-09-07 09:54:24 +0900

    initial commit

---
+++ app/src/main/java/com/example/a507/myapplication0905/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.a507.myapplication0905;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    protected Button button;
+    protected TextView textView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
